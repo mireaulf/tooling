a4c1e993d6e97daff492b4b80b2c738d60ce0998
Francis Mireault <mireaulf@google.com>
2023-06-08 15:37:26 -0400

    Add osx utils

--- osx/init.sh
+++ osx/init.sh
@@ -4,13 +4,18 @@
 
 # zsh
 brew install zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
 if [ "$(uname -m)" == "arm64" ]; then
     chsh -s /opt/homebrew/bin/zsh
-    echo "export PATH=/opt/homebrew/bin:$PATH" > ~/.zshrc
+    echo "export PATH=/opt/homebrew/bin:$PATH" >> ~/.zshrc
     source ~/.zshrc
 else 
     chsh -s /usr/local/bin/zsh
 fi
 
 # python
-brew install pyenv
+brew install pyenv poetry 
+
+# cloud
+brew install kubectl awscli aws-sam-cli
