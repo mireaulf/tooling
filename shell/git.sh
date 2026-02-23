f81307e383110a84a2901107cc07f0cffb5e1f56
FM <mireaulf@gmail.com>
2026-02-23 10:27:28 -0500

    feat: add ciaclean alias to git.sh
    Added a new alias for cleaning up merged branches.

--- shell/git.sh
+++ shell/git.sh
@@ -1,1 +1,4 @@
 alias npr='f() { git checkout -b $1; git push --set-upstream origin $1; }; f'
+
+# source: https://spencer.wtf/2026/02/20/cleaning-up-merged-git-branches-a-one-liner-from-the-cias-leaked-dev-docs.html
+alias ciaclean='git branch --merged origin/main | grep -vE "^\s*(\*|main|develop)" | xargs -n 1 git branch -d'
